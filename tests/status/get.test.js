2bf5b6a24c123a539663f8db5fb9dc5ff6d76e70
Maurício Xavier Oliveira <119255200+mini-niko@users.noreply.github.com>
2024-12-30 20:56:53 +0000

    add `/api/status` endpoint

--- tests/status/get.test.js
+++ tests/status/get.test.js
@@ -2,4 +2,26 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const body = await response.json();
+
+  const parsedDate = new Date(body.updated_at).toISOString();
+
+  expect(body.updated_at).toEqual(parsedDate);
+
+  const databaseInfo = body.dependecies.database;
+
+  expect(databaseInfo.status).toBe("healthy");
+  expect(databaseInfo.version).toBe("16.0");
+  expect(databaseInfo.max_connections).toBe(100);
+  expect(databaseInfo.open_connections).toBe(1);
+});
+
+test.only("Teste de SQL Injection", async () => {
+  const response = await fetch(
+    "http://localhost:3000/api/v1/status?database_name='; SELECT pg_sleep(4) ; --",
+  );
+
+  const body = await response.json();
+  console.log(body);
 });
