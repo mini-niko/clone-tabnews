51cb7b9d672b8336037f1533935966f2f0398d96
mini-niko <xavierdeoliveiramauricio@gmail.com>
2025-05-03 08:45:16 -0300

    feat: create `ServiceError` error

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,11 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno não esperado aconteceu.", {
       cause,
     });
 
     this.name = "InternalServerError";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
   }
 
   toJSON() {
@@ -25,6 +25,27 @@ export class MethodNotAllowedError extends Error {
     this.name = "MethodNotAllowed";
     this.message = "Metodo não permitido para este endpoint.";
     this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: "Verifique se o método HTTP enviado é válido para este endpoint.",
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ message, cause }) {
+    super(message || "Serviço indisponível no momento", {
+      cause,
+    });
+
+    this.name = "ServiceError";
+    this.message = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
   }
 
   toJSON() {
