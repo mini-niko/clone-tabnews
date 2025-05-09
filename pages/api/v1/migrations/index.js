82c62192c7d379f7f65cacce49539df75ec6473b
mini-niko <xavierdeoliveiramauricio@gmail.com>
2025-05-09 15:45:07 -0300

    refactor: use `infra/migrator.js` in `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,45 +1,22 @@
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
 import database from "infra/database.js";
 import createCustomRouter from "infra/router";
+import migrator from "infra/migrator";
 
 export default createCustomRouter({
   getHandler,
   postHandler,
 });
 
-const migrationRunnerOptions = {
-  dir: resolve(process.cwd(), "infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
 async function getHandler(req, res) {
-  const dbClient = await database.getNewClient();
-
-  const pendingMigrations = await migrationRunner({
-    ...migrationRunnerOptions,
-    dbClient,
-    dryRun: true,
-  });
-
-  await dbClient.end();
+  const pendingMigrations = await migrator.listPendingMigrations();
 
   res.status(200).json(pendingMigrations);
 }
 
 async function postHandler(req, res) {
-  const dbClient = await database.getNewClient();
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-  const migratedMigrations = await migrationRunner({
-    ...migrationRunnerOptions,
-    dbClient,
-  });
+  const status = migratedMigrations.length === 0 ? 200 : 201;
 
-  await dbClient.end();
-
-  const method = migratedMigrations.length === 0 ? 200 : 201;
-
-  res.status(method).json(migratedMigrations);
+  res.status(status).json(migratedMigrations);
 }
